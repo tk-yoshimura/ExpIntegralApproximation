dee4f2135bc8a2d327aaa69b19f6c2baf47a4a9c
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-31 02:53:36 +0900

    + negative nearzero

--- ExpIntegral/ExpIntegral.cs
+++ ExpIntegral/ExpIntegral.cs
@@ -42,5 +42,44 @@ namespace ExpIntegral {
                 return MultiPrecision<N>.NaN;
             }
         }
+
+        public static MultiPrecision<N> NegativeNearZero<N, M>(MultiPrecision<N> x, int max_terms = 256) where N : struct, IConstant where M : struct, IConstant {
+            if (!(x <= 0)) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
+            MultiPrecision<M> x_ex = -x.Convert<M>(), x2 = x_ex * x_ex;
+            
+            MultiPrecision<M> s = MultiPrecision<M>.EulerGamma + MultiPrecision<M>.Log(x_ex);
+            MultiPrecision<M> u = -x_ex;
+
+            long k = 0;
+
+            for (long conv_times = 0; k < max_terms && conv_times < 3; k++) {
+                MultiPrecision<M> f = TaylorSeries<M>.Value(checked((int)(2 * k + 1))) * (MultiPrecision<M>.Rcp(2 * k + 1) - x_ex / checked((2 * k + 2) * (2 * k + 2)));
+                MultiPrecision<M> ds = u * f;
+
+                if (ds.Exponent < s.Exponent - MultiPrecision<N>.Bits) {
+                    conv_times++;
+                }
+                else {
+                    conv_times = 0;
+                }
+
+                s += ds;
+                u *= x2;
+
+                if (s.Exponent > MultiPrecision<M>.Bits - MultiPrecision<N>.Bits) {
+                    return MultiPrecision<N>.NaN;
+                }
+            }
+
+            if (k < max_terms) {
+                return s.Convert<N>();
+            }
+            else {
+                return MultiPrecision<N>.NaN;
+            }
+        }
     }
 }
--- ExpIntegral/Program.cs
+++ ExpIntegral/Program.cs
@@ -4,6 +4,11 @@ using System;
 namespace ExpIntegral {
     internal class Program {
         static void Main(string[] args) {
+            for (MultiPrecision<Pow2.N8> x = -32; x < 0; x += 0.125d) {
+                MultiPrecision<Pow2.N8> y = ExpIntegral.NegativeNearZero<Pow2.N8, Pow2.N32>(x);
+
+                Console.WriteLine($"{x},{y}");
+            }
             for (MultiPrecision<Pow2.N8> x = 0; x <= 32; x += 0.125d) {
                 MultiPrecision<Pow2.N8> y = ExpIntegral.PositiveNearZero<Pow2.N8, Pow2.N32>(x);
 
