88508ca1cf01d2985cd1ba9ed15f02d438c96e0e
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-31 03:28:25 +0900

    + negative limit

--- ExpIntegral/ExpIntegral.cs
+++ ExpIntegral/ExpIntegral.cs
@@ -116,5 +116,40 @@ namespace ExpIntegral {
                 return MultiPrecision<N>.NaN;
             }
         }
+
+        public static MultiPrecision<N> NegativeLimit<N>(MultiPrecision<N> x, int max_terms = 256) where N : struct, IConstant {
+            if (!(x <= 0)) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
+            MultiPrecision<N> s = 0;
+            MultiPrecision<N> v = -1 / x, v2 = v * v;
+            MultiPrecision<N> ds = -MultiPrecision<N>.Exp(x) * v;
+
+            long k = 0;
+
+            for (long conv_times = 0; k < max_terms && conv_times < 3; k++) {
+                if (ds.Exponent < s.Exponent - MultiPrecision<N>.Bits) {
+                    conv_times++;
+                }
+                else {
+                    conv_times = 0;
+                }
+
+                if ((2 * k + 1) * v >= 1) { 
+                    return MultiPrecision<N>.NaN;
+                }
+
+                s += ds * (1 - (2 * k + 1) * v);
+                ds *= v2 * (2 * k + 1) * (2 * k + 2);
+            }
+
+            if (k < max_terms) {
+                return s.Convert<N>();
+            }
+            else {
+                return MultiPrecision<N>.NaN;
+            }
+        }
     }
 }
--- ExpIntegral/Program.cs
+++ ExpIntegral/Program.cs
@@ -4,7 +4,12 @@ using System;
 namespace ExpIntegral {
     internal class Program {
         static void Main(string[] args) {
-            for (MultiPrecision<Pow2.N8> x = -32; x < 0; x += 0.125d) {
+            for (MultiPrecision<Pow2.N4> x = -6400; x <= -100; x /= 2) {
+                MultiPrecision<Pow2.N4> y = ExpIntegral.NegativeLimit(x);
+
+                Console.WriteLine($"{x},{y}");
+            }
+            for (MultiPrecision<Pow2.N8> x = -100; x < 0; x += 0.125d) {
                 MultiPrecision<Pow2.N8> y = ExpIntegral.NegativeNearZero<Pow2.N8, Pow2.N32>(x);
 
                 Console.WriteLine($"{x},{y}");
