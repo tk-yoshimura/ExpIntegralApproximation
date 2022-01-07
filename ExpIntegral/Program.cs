7cd917621f32ed443497600a49abab7dd9c09685
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-08 02:46:58 +0900

    + sincosintegral

--- ExpIntegral/Program.cs
+++ ExpIntegral/Program.cs
@@ -7,91 +7,28 @@ namespace ExpIntegral {
     internal class Program {
         static void Main(string[] args) {
 
-            static MultiPrecision<Pow2.N16> invg16(MultiPrecision<Pow2.N16> v) {
-                if (v.IsZero) {
-                    return -1;
-                }
-                MultiPrecision<Pow2.N16> x = 1 / v;
-                MultiPrecision<Pow2.N16> y = ExpIntegralN16.G(x) - x;
-            
-                return y;
-            };
+            for (MultiPrecision<Pow2.N4> x = 1d / 32; x <= 16; x += 1d / 32) {
+                MultiPrecision<Pow2.N4> y = SinCosIntegral.CosNearZero<Pow2.N4, Pow2.N8>(x);
 
-            static MultiPrecision<Pow2.N32> invg32(MultiPrecision<Pow2.N32> v) {
-                if (v.IsZero) {
-                    return -1;
-                }
-                MultiPrecision<Pow2.N32> x = 1 / v;
-                MultiPrecision<Pow2.N32> y = ExpIntegralN32.G(x) - x;
-            
-                return y;
-            };
-                        
-            using (StreamWriter sw = new("../../../../results/ei_invg_pade_table_2.csv")) {            
-                for (MultiPrecision<Pow2.N16> x = -8; x <= 8;) {
-                    bool is_e30 = false;
+                Console.WriteLine($"{x},{y}");
+            }
 
-                    MultiPrecision<Pow2.N16> ddx = Math.ScaleB(1, -2);
-                    for (; ddx >= Math.ScaleB(1, -9) && !is_e30; ddx /= 2) {
-                        MultiPrecision<Pow2.N32>[] ds = FiniteDifference<Pow2.N32>.Diff(
-                            x.Convert<Pow2.N32>(), invg32, Math.ScaleB(1, -24)
-                        );
+            for (MultiPrecision<Pow2.N4> x = 32; x <= 64; x += 1d / 32) {
+                MultiPrecision<Pow2.N4> y = SinCosIntegral.Limit(x).ci;
 
-                        List<MultiPrecision<Pow2.N16>> expecteds = new();
+                Console.WriteLine($"{x},{y}");
+            }
 
-                        for (MultiPrecision<Pow2.N16> dx = -ddx, h = ddx / 512; dx <= ddx; dx += h) {
-                            MultiPrecision<Pow2.N16> expected = invg16(x + dx);
+            for (MultiPrecision<Pow2.N4> x = 1d / 32; x <= 16; x += 1d / 32) {
+                MultiPrecision<Pow2.N4> y = SinCosIntegral.SinNearZero<Pow2.N4, Pow2.N8>(x);
 
-                            expecteds.Add(expected);
-                        }
+                Console.WriteLine($"{x},{y}");
+            }
 
-                        for (int n = 4; n <= 16; n += 1) {
-                            MultiPrecision<Pow2.N16>[] cs = new MultiPrecision<Pow2.N16>[n * 2 + 1];
-                            cs[0] = invg16(x);
-                            for (int i = 0; i < n * 2; i++) {
-                                cs[i + 1] = ds[i].Convert<Pow2.N16>() * MultiPrecision<Pow2.N16>.TaylorSequence[i + 1];
-                            }
+            for (MultiPrecision<Pow2.N4> x = 32; x <= 64; x += 1d / 32) {
+                MultiPrecision<Pow2.N4> y = SinCosIntegral.Limit(x).si;
 
-                            (MultiPrecision<Pow2.N16>[] ms, MultiPrecision<Pow2.N16>[] ns) = PadeSolver<Pow2.N16>.Solve(cs, n, n);
-
-                            MultiPrecision<Pow2.N16> err = 0;
-
-                            for ((MultiPrecision<Pow2.N16> dx, MultiPrecision<Pow2.N16> h, int i) = (-ddx, ddx / 512, 0); i < expecteds.Count; dx += h, i++) {
-                                MultiPrecision<Pow2.N16> expected = expecteds[i];
-                                MultiPrecision<Pow2.N16> actual = PadeSolver<Pow2.N16>.Approx(dx, ms, ns);
-
-                                err = MultiPrecision<Pow2.N16>.Max(err, MultiPrecision<Pow2.N16>.Abs(expected / actual - 1));
-                            }
-
-                            Console.WriteLine($"x={x}, n={n}, |dx| = {ddx}");
-                            Console.WriteLine($"relative error = {err:e10}");
-
-                            if (err > 1e-8) {
-                                break;
-                            }
-
-                            if (err < 1e-30) {
-                                sw.WriteLine($"x={x}, n={n}, |dx| = {ddx}");
-
-                                sw.WriteLine($"i,p_i,q_i");
-                                for (int i = 0; i <= n; i++) {
-                                    sw.WriteLine($"{i},{ms[i]:e64},{ns[i]:e64}");
-                                }
-
-                                sw.WriteLine($"relative error = {err:e10}\n");
-
-                                is_e30 = true;
-
-                                sw.Flush();
-
-                                break;
-                            }
-                        }
-                    }
-
-                    ddx *= 2;
-                    x = ddx * MultiPrecision<Pow2.N16>.Truncate((x + ddx) / ddx);
-                }
+                Console.WriteLine($"{x},{y}");
             }
 
             Console.WriteLine("END");
---
+++ ExpIntegral/SinCosIntegral.cs
@@ -0,0 +1,133 @@
+﻿using MultiPrecision;
+using System;
+
+namespace ExpIntegral {
+    internal static class SinCosIntegral {
+
+        public static MultiPrecision<N> CosNearZero<N, M>(MultiPrecision<N> x, int max_terms = 512) where N : struct, IConstant where M : struct, IConstant {
+            if (!(x >= 0)) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
+            MultiPrecision<M> x_ex = x.Convert<M>(), x2 = x_ex * x_ex, x4 = x2 * x2;
+
+            MultiPrecision<M> s = MultiPrecision<M>.EulerGamma + MultiPrecision<M>.Log(x_ex);
+            MultiPrecision<M> u = -x2;
+
+            long k = 0;
+
+            for (long conv_times = 0; k < max_terms && conv_times < 3; k++) {
+                MultiPrecision<M> f = TaylorSeries<M>.Value(checked((int)(4 * k + 2)))
+                    * (MultiPrecision<M>.Rcp(4 * k + 2) - x2 / checked((4 * k + 3) * (4 * k + 4) * (4 * k + 4)));
+                MultiPrecision<M> ds = u * f;
+
+                if (ds.Exponent < s.Exponent - MultiPrecision<N>.Bits) {
+                    conv_times++;
+                }
+                else {
+                    conv_times = 0;
+                }
+
+                s += ds;
+                u *= x4;
+
+                if (s.Exponent > MultiPrecision<M>.Bits - MultiPrecision<N>.Bits) {
+                    return MultiPrecision<N>.NaN;
+                }
+            }
+
+            if (k < max_terms) {
+                return s.Convert<N>();
+            }
+            else {
+                return MultiPrecision<N>.NaN;
+            }
+        }
+
+        public static MultiPrecision<N> SinNearZero<N, M>(MultiPrecision<N> x, int max_terms = 512) where N : struct, IConstant where M : struct, IConstant {
+            if (!(x >= 0)) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
+            MultiPrecision<M> x_ex = x.Convert<M>(), x2 = x_ex * x_ex, x4 = x2 * x2;
+
+            MultiPrecision<M> s = 0;
+            MultiPrecision<M> u = x_ex;
+
+            long k = 0;
+
+            for (long conv_times = 0; k < max_terms && conv_times < 3; k++) {
+                MultiPrecision<M> f = TaylorSeries<M>.Value(checked((int)(4 * k + 1)))
+                    * (MultiPrecision<M>.Rcp(4 * k + 1) - x2 / checked((4 * k + 2) * (4 * k + 3) * (4 * k + 3)));
+                MultiPrecision<M> ds = u * f;
+
+                if (ds.Exponent < s.Exponent - MultiPrecision<N>.Bits) {
+                    conv_times++;
+                }
+                else {
+                    conv_times = 0;
+                }
+
+                s += ds;
+                u *= x4;
+
+                if (s.Exponent > MultiPrecision<M>.Bits - MultiPrecision<N>.Bits) {
+                    return MultiPrecision<N>.NaN;
+                }
+            }
+
+            if (k < max_terms) {
+                return s.Convert<N>();
+            }
+            else {
+                return MultiPrecision<N>.NaN;
+            }
+        }
+
+
+        public static (MultiPrecision<N> ci, MultiPrecision<N> si) Limit<N>(MultiPrecision<N> x, int max_terms = 256) where N : struct, IConstant {
+            if (!(x >= 0)) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
+            MultiPrecision<N> v = 1 / x, v2 = v * v, v4 = v2 * v2;
+            MultiPrecision<N> f = 1;
+
+            MultiPrecision<N> p = 0, q = 0;
+            MultiPrecision<N> c = 1, d = v;
+            
+            long k = 0;
+            for (; k < max_terms; k++) {
+                MultiPrecision<N> dp = f * c * (1 - checked((4 * k + 1) * (4 * k + 2)) * v2);
+                MultiPrecision<N> dq = f * (4 * k + 1) * d * (1 - checked((4 * k + 2) * (4 * k + 3)) * v2);
+
+                if (dp < 0 || dq < 0) {
+                    return (MultiPrecision<N>.NaN, MultiPrecision<N>.NaN);
+                }
+                if ((dp.Exponent < p.Exponent - MultiPrecision<N>.Bits) && (dq.Exponent < q.Exponent - MultiPrecision<N>.Bits)) {
+                    break;
+                }
+
+                p += dp;
+                q += dq;
+                c *= v4;
+                d *= v4;
+                f *= (4 * k + 1) * (4 * k + 2);
+                f *= (4 * k + 3) * (4 * k + 4);
+
+            }
+
+            if (k >= max_terms) {
+                return (MultiPrecision<N>.NaN, MultiPrecision<N>.NaN);
+            }
+
+            MultiPrecision<N> cos = MultiPrecision<N>.Cos(x) / x;
+            MultiPrecision<N> sin = MultiPrecision<N>.Sin(x) / x;
+
+            MultiPrecision<N> si = -cos * p - sin * q;
+            MultiPrecision<N> ci = sin * p - cos * q;
+
+            return (si, ci);
+        }
+    }
+}
