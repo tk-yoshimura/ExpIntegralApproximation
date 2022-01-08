c5768dd2bac2d25d8ae2aa67138da8556a35d31c
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-08 13:42:18 +0900

    + sincosintegral n8

--- ExpIntegral/Program.cs
+++ ExpIntegral/Program.cs
@@ -6,22 +6,22 @@ namespace ExpIntegral {
     internal class Program {
         static void Main(string[] args) {
 
-            using (StreamWriter sw = new("../../../../results/cisi_table_n4.csv")) {
+            using (StreamWriter sw = new("../../../../results/cisi_table_N8.csv")) {
                 sw.WriteLine("x,Ci,Si");
 
-                for (MultiPrecision<Pow2.N4> x = 0; x <= 256; x += 1 / 32d) {
-                    (MultiPrecision<Pow2.N4> ci, MultiPrecision<Pow2.N4> si) = SinCosIntegralN4.Value(x);
+                for (MultiPrecision<Pow2.N8> x = 0; x <= 256; x += 1) {
+                    (MultiPrecision<Pow2.N8> ci, MultiPrecision<Pow2.N8> si) = SinCosIntegralN8.Value(x);
 
                     sw.WriteLine($"{x},{ci},{si}");
                     Console.WriteLine($"{x},{ci},{si}");
                 }
             }
 
-            using (StreamWriter sw = new("../../../../results/cisi_fg_n4.csv")) {
+            using (StreamWriter sw = new("../../../../results/cisi_fg_N8.csv")) {
                 sw.WriteLine("x,f,g");
 
-                for (MultiPrecision<Pow2.N4> x = 0; x <= 256; x += 1 / 32d) {
-                    (MultiPrecision<Pow2.N4> f, MultiPrecision<Pow2.N4> g) = SinCosIntegralN4.FG(x);
+                for (MultiPrecision<Pow2.N8> x = 0; x <= 256; x += 1) {
+                    (MultiPrecision<Pow2.N8> f, MultiPrecision<Pow2.N8> g) = SinCosIntegralN8.FG(x);
 
                     sw.WriteLine($"{x},{f},{g}");
                     Console.WriteLine($"{x},{f},{g}");
---
+++ ExpIntegral/SinCosIntegralN8.cs
@@ -0,0 +1,40 @@
+﻿using MultiPrecision;
+
+namespace ExpIntegral {
+    internal class SinCosIntegralN8 {
+        const int threshold = 183;
+
+        public static (MultiPrecision<Pow2.N8> ci, MultiPrecision<Pow2.N8> si) Value(MultiPrecision<Pow2.N8> x) {
+            if (x <= threshold) {
+                MultiPrecision<Pow2.N8> ci = SinCosIntegral.CosNearZero<Pow2.N8, Pow2.N16>(x);
+                MultiPrecision<Pow2.N8> si = SinCosIntegral.SinNearZero<Pow2.N8, Pow2.N16>(x, offset: true);
+
+                return (ci, si);
+            }
+            else {
+                (MultiPrecision<Pow2.N8> ci, MultiPrecision<Pow2.N8> si) = SinCosIntegral.Limit(x);
+
+                return (ci, si);
+            }
+        }
+
+        public static (MultiPrecision<Pow2.N8> f, MultiPrecision<Pow2.N8> g) FG(MultiPrecision<Pow2.N8> x) {
+            if (x <= threshold) {
+                MultiPrecision<Pow2.N8> ci = SinCosIntegral.CosNearZero<Pow2.N8, Pow2.N16>(x);
+                MultiPrecision<Pow2.N8> si = SinCosIntegral.SinNearZero<Pow2.N8, Pow2.N16>(x, offset: false);
+
+                MultiPrecision<Pow2.N8> cos = MultiPrecision<Pow2.N8>.Cos(x), sin = MultiPrecision<Pow2.N8>.Sin(x);
+
+                MultiPrecision<Pow2.N8> f = ci * sin - si * cos;
+                MultiPrecision<Pow2.N8> g = -ci * cos - si * sin;
+
+                return (f, g);
+            }
+            else {
+                (MultiPrecision<Pow2.N8> f, MultiPrecision<Pow2.N8> g) = SinCosIntegral.LimitFG(x);
+
+                return (f, g);
+            }
+        }
+    }
+}
